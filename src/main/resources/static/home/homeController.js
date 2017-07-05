b103a937c636fc743f6cd66d4df14f4ea84b9b7d
Scott <scott@nowhere.com>
2017-07-05 07:45:43 +0100

    More updates

--- src/main/resources/static/home/homeController.js
+++ src/main/resources/static/home/homeController.js
@@ -1,6 +1,7 @@
 mainApp.controller('homeController', function($scope, filmsApiService) {
 	
 	$scope.films = [];
+	$scope.selected = {};
 	
 	filmsApiService.getFilms().then(
 		function(data) {
@@ -10,5 +11,14 @@ mainApp.controller('homeController', function($scope, filmsApiService) {
 			console.log("Failed to get list of films: " + error);
 		}
 	);
+	
+	// Try uncommenting the following line. You should get an HTTP error
+	// as the back-end doesn't support doing DELETE yet!
+	//
+	// filmsApiService.deleteFilm("1");
+	
+	$scope.doSelectFilm = function(film) {
+		$scope.selected = film;
+	}
 	
 });
--- src/main/resources/static/services/filmsApiService.js
+++ src/main/resources/static/services/filmsApiService.js
@@ -1,31 +1,93 @@
 mainApp.factory("filmsApiService", ["$http", "$q",
     function($http, $q) {
 
+		/**
+		 * Do a GET Rest call
+		 */
+		function doGet(url) {
+			// Need to use a promise as it takes time to call the back-end.
+			// The promise is resolved once the response is received.
+			var deferred = $q.defer();
+
+			var op = "GET " + url;
+			
+			$http.get(url).then(
+				function(response) {
+					// REST call was successful
+					console.log(op + " success - HTTP Status: " + response.status);
+					deferred.resolve(response.data);
+		        }, function(error) {
+					// REST call failed
+		        	var status = error.status;
+		        	var errMsg = error.data.message || "No error message";
+		        	console.error(op + " failed - HTTP Status: " + status + ", Error: " + errMsg);
+		        	deferred.reject(status);
+		        }
+		    );
+			
+			return deferred.promise;
+		}
+	
+		/**
+		 * Do a DELETE Rest call
+		 */
+		function doDelete(url) {
+			// Need to use a promise as it takes time to call the back-end.
+			// The promise is resolved once the response is received.
+			var deferred = $q.defer();
+
+			var op = "DELETE " + url;
+			
+			$http.delete(url).then(
+				function(response) {
+					// REST call was successful
+					console.log(op + " success - HTTP Status: " + response.status);
+					deferred.resolve(response.data);
+		        }, function(error) {
+					// REST call failed
+		        	var status = error.status;
+		        	var errMsg = error.data.message || "No error message";
+		        	console.error(op + " failed - HTTP Status: " + status + ", Error: " + errMsg);
+		        	deferred.reject(status);
+		        }
+		    );
+			
+			return deferred.promise;
+		}
+
+		/**
+		 * Do a POST Rest call
+		 */
+		function doPost(url, body) {
+			//
+			// Write Me!
+			//
+			// This is similar to the above calls but you need to pass the body also.
+			//
+			// Use JSON.stringify to convert the body from an object into JSON.
+			//
+			// e.g.
+			//
+			// var body = { "title": "My Title", "year": "2017" };
+			// var bodyData = JSON.stringify(body);
+			//
+			// $http.post(url, bodyData).then(...
+		}
+		
 		return {
 
+			// This is where we make all our back-end REST calls
+			
 			getFilms: function() {
-				// Need to use a promise as it takes time to call the back-end.
-				// The promise is resolved once the response is received.
-				var deferred = $q.defer();
-				
-				var url = "/films";
-				var op = "GET " + url;
-				
-				$http.get(url).then(
-					function(response) {
-						// REST call was successful
-						console.log(op + " success - HTTP Status: " + response.status);
-						deferred.resolve(response.data);
-			        }, function(error) {
-						// REST call failed
-			        	var status = error.status;
-			        	var errMsg = error.data.message || "No error message";
-			        	console.error(op + " failed - HTTP Status: " + status + ", Error: " + errMsg);
-			        	deferred.reject(status);
-			        }
-			    );
-				
-				return deferred.promise;
+				return doGet("/films");
+			},
+			
+			getFilm: function(id) {
+				return doGet("/films/" + id);
+			},
+			
+			deleteFilm: function(id) {
+				return doDelete("/films/" + id);
 			}
 			
 		};
